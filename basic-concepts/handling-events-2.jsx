0c595e227b771d046ac8a61ae10fb12a2aa7991c
Gabriele <gabri46nicotra@gmail.com>
2024-03-14 18:21:39 +0100

    exercises basic concepts

---
+++ basic-concepts/handling-events-2.jsx
@@ -0,0 +1,14 @@
+export function MouseClicker() {
+    function eventButtonClick(event) {
+        console.log(event.target.name)
+    }
+    function eventImageClick(event) {
+        console.log(event.target.src)
+    }
+
+    return (<button name='one' onClick={eventButtonClick}>
+        <img src="https://www.google.com" alt="" width={20} height={20} onClick={eventImageClick} />
+        Click
+    </button>
+    )
+}
